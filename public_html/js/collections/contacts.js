69ab6c3afea69f9f4f2f72f1e3d680e6ec21e04a
Monica Salamanca <moni.salamanca@gmail.com>
2015-06-01 21:16:17 +0200

    added a new attribute to the model, and coded the collection. A basic collection, we might add more to the collection as we go in my example

--- public_html/js/collections/contacts.js
+++ public_html/js/collections/contacts.js
@@ -1,7 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+var app = app || {};
+
+// Contacts book collection
+// -----------------------
+
+// The collection of the contacts book is backed by **localStorage** instead
+// of using a remote server
+var ContactsList = Backbone.Collection.extend({
+  
+  // Needs to reference the collection's model
+  model: app.Contact,
+  
+  // save all the contacts under the 'contacts-backbone' namespace.
+  localStorage: new Backbone.LocalStorage('contacts-backbone')
+  
+  
+});
+
+
+// Create our global collection of **Contacts**
+app.Contacts = new ContactsList();
 
--- public_html/js/models/contacts.js
+++ public_html/js/models/contacts.js
@@ -9,10 +9,11 @@ var app = app || {};
 app.Contact = Backbone.Model.extend({
   
   // I need to use defaults, to make sure that every contact created, has
-  // a name and a phone number keys
+  // a name and a phone number and the genre keys
   defaults: {
     name: '',
-    phone: ''
+    phone: '',
+    genre: ''
   }
   
 });
