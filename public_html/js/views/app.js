e2bcd9c6c65cd261bc7bbf95ca5a9565c074f7fc
Monica Salamanca <moni.salamanca@gmail.com>
2015-06-02 21:30:39 +0200

    created the main view for the contacts book

--- public_html/js/views/app.js
+++ public_html/js/views/app.js
@@ -1,7 +1,18 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+var app = app || {};
+
+// The Application
+// ---------------
+
+// Our overall **Appview** is the top level piece of the UI
+app.AppView = Backbone.View.extend({
+  
+  // Instead of generating a new element, bind to the existing skeleton of the 
+  // App already presents in the HTML
+  el: '#my-contacts'
+  
+  // TODO the bottom of the contacts to organize by gender and/or alphabetical
+  
+  
+});
 
