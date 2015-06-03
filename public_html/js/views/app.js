cb699ca224914ef61834c5cf93a8bcbd9124bf15
Monica Salamanca <moni.salamanca@gmail.com>
2015-06-03 22:36:10 +0200

    added an id to the button that will add the contacts and added the functionality to the view

--- public_html/js/views/app.js
+++ public_html/js/views/app.js
@@ -9,10 +9,26 @@ app.AppView = Backbone.View.extend({
   
   // Instead of generating a new element, bind to the existing skeleton of the 
   // App already presents in the HTML
-  el: '#my-contacts'
+  el: '#my-contacts',
   
   // TODO the bottom of the contacts to organize by gender and/or alphabetical
+  // bottomTemplate = _.template .... call the bottom template which will have the filters
   
+  // At initialization we bind to the relevant events on the 'Todos'
+  // collection, when items are added or changed
+  initialize: function() {
+    
+    this.$button = this.$('#add-contact');
+    
+    this.listentTo(app.Contacts, 'add', this.addContact);
+  },
+  
+  // Add a single contact to the list by creating a view for it, and appending
+  // its elements to the '<ul>'
+  addContact: function( contact ) {
+    var view = new app.ContactView({ model: contact });
+    $('#contacts-list').append( view.render().el );
+  }
   
 });
 
