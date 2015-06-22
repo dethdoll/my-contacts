01a991f11e148cac0c54bd4828c61ccf93ab4517
Monica Salamanca <moni.salamanca@gmail.com>
2015-06-22 21:57:23 +0200

    edit name and phone number.

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -2,6 +2,8 @@
 
 var app = app || {};
 
+var ENTER_KEY = 13;
+
 $(function() {
   
   new app.AppView();
--- public_html/js/views/contacts.js
+++ public_html/js/views/contacts.js
@@ -15,7 +15,11 @@ app.ContactView = Backbone.View.extend({
   
   // The DOM events specific to a contact
   events: {
-    'click .destroy': 'clear'
+    'click .destroy': 'clear',
+    'dblclick label.name': 'editName',
+    'dblclick label.phone': 'editPhone',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
   },
   
   // The ContactView listens for changes to its model, re-rendering. Since
@@ -23,15 +27,47 @@ app.ContactView = Backbone.View.extend({
   // **ContactView** in this app, we will set a direct reference for experience
   initialize: function() {
     this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'change', this.render);
   },
   
-  // Re-renders the titles of the conact Item
+  // Re-renders the titles of the contact Item
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
-    
+    this.$inputname = this.$('.edit.name');
+    this.$inputphone = this.$('.edit.phone');
     return this;
   },
   
+  // switch this view into 'editing' mode, displaying the input field
+  editName: function() {
+    this.$el.addClass('editing');
+    this.$inputname.focus();
+  },
+  
+    // switch this view into 'editing' mode, displaying the input field
+  editPhone: function() {
+    this.$el.addClass('editing');
+    this.$inputphone.focus();
+  },
+  
+  // Close the 'editing' mode, saving changes to the contacts
+  close: function() {
+    var value = this.$inputname.val().trim();
+    console.log(value); // this is called twice!
+    
+    if (value) {
+      this.model.save( {name: value} );
+    }
+    
+    this.$el.removeClass('editing');
+  },
+  
+  //If you hit 'enter', we're through editing the contact
+  updateOnEnter: function(e) {
+    if ( e.which === ENTER_KEY ) {
+        this.close();
+    }
+  },
   
   //Remove the contact, destroy the model from the *localStorage* and delete 
   //it's view
